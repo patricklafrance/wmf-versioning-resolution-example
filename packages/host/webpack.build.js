f47a0915397d8ce38665d4ad322c99d020e7ad88
Patrick Lafrance <patricklafrance@users.noreply.github.com>
2024-01-11 11:04:44 -0500

    Initial commit

---
+++ packages/host/webpack.build.js
@@ -0,0 +1,65 @@
+// ts-check
+
+import HtmlWebpackPlugin from "html-webpack-plugin";
+import ModuleFederationPlugin from "webpack/lib/container/ModuleFederationPlugin.js";
+import path from "path";
+
+/** @type {import("webpack").Configuration} */
+export default {
+    mode: "production",
+    target: "web",
+    entry: "./src/index.js",
+    output: {
+        path: path.resolve("dist"),
+        // The trailing / is very important, otherwise paths will ne be resolved correctly.
+        publicPath: "http://localhost:8080/",
+        clean: true
+    },
+    optimization: {
+        minimize: false
+    },
+    module: {
+        rules: [
+            {
+                test: /\.(js|jsx)$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: "swc-loader"
+                }
+            },
+            {
+                // https://stackoverflow.com/questions/69427025/programmatic-webpack-jest-esm-cant-resolve-module-without-js-file-exten
+                test: /\.js/,
+                resolve: {
+                    fullySpecified: false
+                }
+            },
+            {
+                test: /\.(png|jpe?g|gif)$/i,
+                type: "asset/resource"
+            }
+        ]
+    },
+    plugins: [
+        new ModuleFederationPlugin({
+            name: "host",
+            remotes: {
+                remote1: "remote1@http://localhost:8081/remoteEntry.js",
+                remote2: "remote2@http://localhost:8082/remoteEntry.js"
+            },
+            shared: {
+                "react": {
+                  singleton: true,
+                  strictVersion: true
+                },
+                "react-dom": {
+                  singleton: true,
+                  strictVersion: true
+                }
+            }
+        }),
+        new HtmlWebpackPlugin({
+            template: "./public/index.html"
+        })
+    ]
+};
